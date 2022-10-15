aff6a93a5e0e433676ef2773d59f9c9322970b07
Ty Aldis <105513746+Tyler-Aldis@users.noreply.github.com>
2022-10-14 19:01:14 -0700

    first commit

---
+++ Game/game.js
@@ -0,0 +1,47 @@
+/*var userName = ''
+const x = "tyler"
+const Mod = y 
+const outPut2 = 1
+var y = outPut2 + y
+var Output= x + y;
+const mOutput = x || y
+console.log(userName.length)
+console.log(`Hello ${x} welcome`)
+console.log(y)*/
+
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+let userQuestion ='will i save the wirld'
+
+console.log(`${userName} has asked -${userQuestion}`)
+let randomNumber = Math.floor(Math.random() * 8);
+console.log(randomNumber);
+
+let eightBall = ''
+switch(randomNumber){
+  case 0 :
+  eightBall = '' ;
+  break;
+   case 1 :
+  eightBall = '' ;
+  break;
+   case 2 :
+  eightBall = '' ;
+  break;
+   case 3 :
+  eightBall = '' ;
+  break;
+   case 4 :
+  eightBall = '' ;
+  break;
+   case 5 :
+  eightBall = '' ;
+  break;
+   case 6 :
+  eightBall = '' ;
+  break;
+   case 7 :
+  eightBall = '' ;
+  break;
+  
+
+}
