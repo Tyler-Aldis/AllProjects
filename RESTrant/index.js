1242459fd2e62f70e46d6bb8065e5af5015f4411
Ty Aldis <105513746+Tyler-Aldis@users.noreply.github.com>
2022-11-15 15:49:09 -0800

    404 page route
    addes routes for this step
    5. Check your GitHub repository in your browser. Make sure that the node_modules folder and .env file are NOT pushed up to GitHub

--- RESTrant/index.js
+++ RESTrant/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -5,4 +6,25 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+app.listen(process.env.PORT)
+
+/*day one part 3  */
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
+
+/*end day 3 part one */
+/* */
+/*Day 1-3 RestRant*/
+
+/*Day 4-8 RestRant*/
